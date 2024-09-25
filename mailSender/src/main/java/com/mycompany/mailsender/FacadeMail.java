074db184a297c20fb7973a06ea764d8052d1e32c
Carlos H Sánchez <carloshiramsanchez@gmail.com>
2024-09-24 18:50:02 -0700

    Creacion de proyectos
    implementacion de algunas clases also

---
+++ mailSender/src/main/java/com/mycompany/mailsender/FacadeMail.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mailsender;
+
+/**
+ *
+ * @author carlo
+ */
+public class FacadeMail implements iMail {
+
+    @Override
+    public void enviarCorreo() {
+        
+    }
+    
+}
---
+++ mailSender/src/main/java/com/mycompany/mailsender/Mail.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mailsender;
+
+/**
+ *
+ * @author carlo
+ */
+public class Mail {
+    private String subject;
+    private String body;
+    private String email;
+    private String emailTo;
+    
+}
