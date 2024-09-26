f3b4f31323dbef691d3b1e4a5ece48b0c183d2b6
Carlos H Sánchez <carloshiramsanchez@gmail.com>
2024-09-25 23:41:54 -0700

    App Client
    interfaz beta

---
+++ AppClient/src/main/java/com/mycompany/appclient/AppClient.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.appclient;
+
+/**
+ *
+ * @author carlo
+ */
+public class AppClient {
+
+    public static void main(String[] args) {
+        UIAppClient ui = new UIAppClient();
+        ui.setVisible(true);
+    }
+}
